46f9f870022b8ed3841cb3135657defd37114eed
Yush1nk1m <kys010306@sogang.ac.kr>
2024-08-24 02:25:44 +0900

    Update from main repo commit

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -3,7 +3,7 @@ import { getAccessToken, isTokenExpired, setTokens } from "../utils/tokenUtils";
 import { Api } from "./swaggerApi";
 
 const api = new Api({
-  baseUrl: import.meta.env.VITE_API_URL,
+  baseUrl: "http://localhost:4000", //import.meta.env.VITE_API_URL,
   securityWorker: (securityData) => {
     return securityData
       ? { headers: { Authorization: `Bearer ${securityData}` } }
